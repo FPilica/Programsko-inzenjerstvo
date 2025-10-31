b73927f5b6f3e3510659d13fda52a6eafee7f27b
Fran Pilica <61914106+FPilica@users.noreply.github.com>
2025-10-31 19:17:09 +0100

    Add model classes

---
+++ Mindfulness.Server/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mindfulness.Server.Models;
+
+public class Event
+{
+    public Guid Id { get; set; }
+    
+    [MaxLength(30)]
+    public string Title { get; set; }
+    
+    [MaxLength(300)]
+    public string? Description { get; set; }
+    
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+}
