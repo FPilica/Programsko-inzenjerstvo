04e2d22b3359e31c0382e8672bb35c1e35823249
Fran Pilica <61914106+FPilica@users.noreply.github.com>
2025-10-31 13:58:53 +0100

    Remove docker for development

--- Mindfulness.Tests/UnitTest1.cs
+++ Mindfulness.Tests/UnitTest1.cs
@@ -1,16 +1,15 @@
-﻿namespace Mindfulness.Tests
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+﻿namespace Mindfulness.Tests;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
     }
 }
