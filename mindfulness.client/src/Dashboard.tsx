155667e04b3470dc262de74d0f942e642e47adbd
IvanL26 <102292214+IvanL26@users.noreply.github.com>
2025-11-08 17:45:50 +0100

    provjeriti app i auth (#5)
    * nulta verzija frontend
    
    * Dodani fajlovi za frontend stranice, napravljen fajl za routiranje
    
    * dodane stranice za auth i putevi za ostale stranice
    
    * a
    
    * a
    
    * postavljeni auth i ostali
    
    * popravak
    
    * Rename \package.g.props to package.g.props
    
    * Delete mindfulness.client/obj\Debug directory
    
    ---------
    
    Co-authored-by: lovrebaus <baus.lovre@gmail.com>

---
+++ mindfulness.client/src/Dashboard.tsx
@@ -0,0 +1,33 @@
+import './App.css'
+import './Dashboard.css'
+import Header from './components/Header.tsx'
+
+function Dashboard() {
+    return (
+        <>
+            <div className="dashboardContainer">
+            <Header />
+            <h1>Dashboard</h1>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                <p>Welcome to your dashboard!</p>
+                
+            </div>
+        </>
+    )
+}
+
+export default Dashboard
---
+++ mindfulness.client/src/ForgotPass.tsx
@@ -0,0 +1,57 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import {useState} from 'react'
+import './App.css'
+import './Auth.css'
+import logoPurple from './assets/logo_boja 2.png'
+// auth 
+// npx vite u cmd u folder
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+function ForgotPass() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Email za reset lozinke:', email);
+    // logika
+  }
+
+  return (
+    <>
+      
+      <div className='containerForgot'>
+        <Link className="logoLink" to="/">
+          <img src={logoPurple} alt="logo_purple" width="177" height="41" />
+        </Link>
+        <div className="forgotpassInfo">
+          <h3>Zaboravili ste lozinku?</h3>
+          <p>Poslat ćemo Vam link za ponovno postavljanje lozinke na vašu e-mail adresu.</p>
+        </div>
+        <form className="formForgotpass" onSubmit={handleSubmit}>
+          <label htmlFor="email">E-mail*</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            placeholder='Unesi e-mail adresu'
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete='email'
+            required
+          ></input>
+          <div className="buttonsForgotpass">
+            <button className='cancel' type="button" onClick={() => navigate('/auth/login')}>Odustani</button>
+            <button className='submit' type="submit">Pošalji</button>
+          </div>
+        </form>
+
+      </div>
+    </>
+  )
+}
+
+export default ForgotPass
