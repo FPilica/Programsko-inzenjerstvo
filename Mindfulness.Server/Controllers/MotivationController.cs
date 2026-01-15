7f32ba5f3a863c60e020aa0c6eefaeb4dc109e4a
FPilica <Fran.Pilica@fer.hr>
2026-01-15 22:06:05 +0100

    Bring back authorization

--- Mindfulness.Server/Controllers/MotivationController.cs
+++ Mindfulness.Server/Controllers/MotivationController.cs
@@ -7,6 +7,7 @@ using Mindfulness.Server.Models;
 
 namespace Mindfulness.Server.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class MotivationController : ControllerBase
